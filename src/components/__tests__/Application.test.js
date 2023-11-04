29e6838ab1f6fa644f9b006ab8d971367fa2107a
RussellAbraham <russell_a@live.com>
2023-11-03 18:58:10 -0700

    Finished tests in Application.test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -100,16 +100,95 @@ describe("Application", () => {
   });
 
 
-  it("loads data, edits an interview and keeps the spots remaining for Monday the same", () => {
-    
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async() => {
+    //  1. Render the Application.
+    const { container } = render(<Application />);
+  
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+  
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+  
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  
+    fireEvent.click(getByText(appointment, "Save"));
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+  
+    await waitForElement(() => getByText(container, "Sylvia Palmer"))
+    expect(getByText(container, "Sylvia Palmer")).toBeInTheDocument();
+    const day = getAllByTestId(container, "day").find(day => 
+      queryByText(day, "Monday")
+    );
+    expect(getByText(day, "1 spot remaining"))
+  
   });
 
-  it("shows the delete error when failing to delete an existing appointment", ()=> {
-
+  it("shows the delete error when failing to delete an existing appointment", async () => {
+    axios.delete.mockRejectedValueOnce();
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+  
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(queryByAltText(appointment, "Delete"))
+  
+    expect(getByText(appointment, "Are you sure you would like to delete?")).toBeInTheDocument();
+  
+    fireEvent.click(queryByText(appointment, "Confirm"))
+  
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+  
+    await waitForElement(() => getByText(appointment, "Error"));
+    expect(getByText(appointment, "Error")).toBeInTheDocument();
+  
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+  
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
   });
 
-  it("shows the save error when failing to save an appointment", () => {
+  it("shows the save error when failing to save an appointment", async() => {
     axios.put.mockRejectedValueOnce();
+    const { container } = render(<Application />)
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+  
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+  
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  
+    fireEvent.click(getByText(appointment, "Save"));
+  
+  
+    await waitForElement(() => getByText(appointment, "Error"));
+    expect(getByText(appointment, "Error")).toBeInTheDocument();
+    
+    fireEvent.click(queryByAltText(appointment, "Close"));
+  
+    expect(getByText(appointment, "Save")).toBeInTheDocument();
+  
+    fireEvent.click(queryByText(appointment, "Cancel"))
+  
+    expect(getByText(container, "Archie Cohen")).toBeInTheDocument();
+  
+    const day = getAllByTestId(container, "day").find(day =>
+        queryByText(day, "Monday")
+      );
+  
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
   });
 
 
