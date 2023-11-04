10bbcf8b7504bd27ebca96d6137352927f2f2f32
RussellAbraham <russell_a@live.com>
2023-11-03 18:26:48 -0700

    Update Form and Application test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,7 +7,7 @@ import React from "react";
   We import our helper functions from the react-testing-library
   The render function allows us to render Components
 */
-import { render, act } from "@testing-library/react";
+import { fireEvent, render, waitForElement } from "@testing-library/react";
 
 /*
   We import the component that we are testing
@@ -17,8 +17,21 @@ import Application from "components/Application";
 /*
   A test that renders a React Component
 */
-it("renders without crashing", () => {
-  act(() => {
-    render(<Application />);
+describe("Application", () => {
+  
+  //it("renders without crashing", () => {
+  //  act(() => {
+  //    render(<Application />);
+  //  });
+  //});
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+  
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
+
 });
